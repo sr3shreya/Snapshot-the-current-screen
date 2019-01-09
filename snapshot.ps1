560726804f3763aff64b48699a7256054b6e7481
Shreya Srivastava <shreya.srivastava963@gmail.com>
2019-01-09 16:58:09 +0530

    Add files via upload

---
+++ snapshot.ps1
@@ -0,0 +1,22 @@
+﻿[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Outlook) | Out-Null
+[System.Windows.Forms.Sendkeys]::SendWait("{PRTSC}")
+    Start-Sleep -Seconds 2
+    C:\Windows\System32\mspaint.exe
+    Start-Sleep -seconds 6
+    [System.Windows.Forms.Sendkeys]::SendWait("^v")
+    Start-Sleep -Seconds 1
+    [System.Windows.Forms.Sendkeys]::SendWait("^s")
+    Start-Sleep -Seconds 1
+    [System.Windows.Forms.Sendkeys]::SendWait("^s")
+    Start-Sleep -Seconds 1
+    $Request_Report_PaintFile_Name= "whatever the name"
+    [System.Windows.Forms.Sendkeys]::SendWait($Request_Report_PaintFile_Name)
+    Start-Sleep -Seconds 1  
+    [System.Windows.Forms.Sendkeys]::SendWait("{TAB}")
+    Start-Sleep -Seconds 1
+   
+    [System.Windows.Forms.Sendkeys]::SendWait("j")
+    Start-Sleep -Seconds 2
+   
+    [System.Windows.Forms.Sendkeys]::SendWait("{ENTER}")
+    Start-Sleep -Seconds 2
